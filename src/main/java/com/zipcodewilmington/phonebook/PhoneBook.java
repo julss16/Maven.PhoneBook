69ce821dbe7dcca46d6f69c31043f4010eb207cf
ajuluadigwe <oadigwe2@gmail.com>
2019-07-01 12:22:36 -0400

    done!

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,78 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    Map<String, List<String>> map;
+    List<String> list= new ArrayList<>();
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map= map;
     }
 
     public PhoneBook() {
-        this(null);
+       map= new LinkedHashMap<>();
+
     }
 
     public void add(String name, String phoneNumber) {
+
+        if (map.containsKey(name))
+            map.get(name).add(phoneNumber);
+        else {
+            map.put(name, list);
+            add(name, phoneNumber);
+        }
     }
 
     public void addAll(String name, String... phoneNumbers) {
+
+        map.put(name, new ArrayList<>(Arrays.asList(phoneNumbers)));
     }
 
     public void remove(String name) {
+
+            map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+
+        if (map.containsKey(name)){
+            return  true;
+
+        } return false;
     }
 
     public List<String> lookup(String name) {
-        return null;
+
+        return map.get(name);
     }
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
+    public String reverseLookup(String phoneNumber) {
+
+        String n= "";
+        for (Map.Entry<String, List<String>> entry: map.entrySet()){
+            n= entry.getKey();
+
+
+        } return n;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+
+        ArrayList<String> names= new ArrayList<>();
+        for (String n: map.keySet()){
+            names.add(n);
+        }
+        return names;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return map;
     }
 }
